871758ea9cc298925a7e895326d4920fa34772d6
pochen <chenpaul914@gmail.com>
2013-09-24 13:26:17 +1000

    init

---
+++ GameObject.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using SharpDX;
+using SharpDX.Toolkit;
+
+namespace Project1
+{
+    using SharpDX.Toolkit.Graphics;
+    using SharpDX.Toolkit.Input;
+    abstract public class GameObject
+    {
+        public BasicEffect basicEffect;
+        public VertexInputLayout inputLayout;
+        public Game game;
+
+        public abstract void Update(GameTime gametime);
+        public abstract void Draw(GameTime gametime);
+
+        public abstract void Control(KeyboardState keystate);
+    }
+}
---
+++ Project1Game.cs
@@ -0,0 +1,93 @@
+﻿// Copyright (c) 2010-2013 SharpDX - Alexandre Mutel
+// 
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+// 
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+// 
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+// THE SOFTWARE.
+
+using System;
+
+using SharpDX;
+using SharpDX.Toolkit;
+
+namespace Project1
+{
+    // Use this namespace here in case we need to use Direct3D11 namespace as well, as this
+    // namespace will override the Direct3D11.
+    using SharpDX.Toolkit.Graphics;
+    using SharpDX.Toolkit.Input;
+
+    public class Project1Game : Game
+    {
+        private GraphicsDeviceManager graphicsDeviceManager;
+        private GameObject model;
+        private KeyboardManager km;
+        public KeyboardState ks;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="Project1Game" /> class.
+        /// </summary>
+        public Project1Game()
+        {
+            // Creates a graphics manager. This is mandatory.
+            graphicsDeviceManager = new GraphicsDeviceManager(this);
+
+            // Setup the relative directory to the executable directory
+            // for loading contents with the ContentManager
+            Content.RootDirectory = "Content";
+
+            // Creates a keyboard manager
+            km = new KeyboardManager(this);
+        }
+
+        protected override void LoadContent()
+        {
+            model = new Landscape(this);
+
+            // Create an input layout from the vertices
+
+            base.LoadContent();
+        }
+
+        protected override void Initialize()
+        {
+            Window.Title = "Project 1";
+
+            base.Initialize();
+        }
+
+        protected override void Update(GameTime gameTime)
+        {
+            ks = km.GetState();
+            model.Update(gameTime);
+            model.Control(ks);
+
+            // Handle base.Update
+            base.Update(gameTime);
+        }
+
+        protected override void Draw(GameTime gameTime)
+        {
+            // Clears the screen with the Color.CornflowerBlue
+            GraphicsDevice.Clear(Color.Black);
+
+            model.Draw(gameTime);
+
+            // Handle base.Draw
+            base.Draw(gameTime);
+        }
+    }
+}
