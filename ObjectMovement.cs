7df63293d838d06198b48d601772109081b7cb38
Litao(Terry) SHEN <litaos@student.unimelb.edu.au>
2013-10-12 16:54:40 +1100

    object normal move

--- ObjectMovement.cs
+++ ObjectMovement.cs
@@ -17,25 +17,47 @@ namespace Project2
         Project2Game game;
         Landscape2 land;
 
-        float v0 = 0.01f; 
+        // initial velocity
+        float v0; 
         Vector3 direction;
-        Vector3 acc;
+        // accelerate 
+        float acc;
+        float cos;
+        float sin;
+        // the longest side
+        float c;
+        // radius
+        float r = 0.08f;
 
         public ObjectMovement(Project2Game game)
         {
             this.game = game;
             land = new Landscape2(game);
             direction = new Vector3(1,1,1);
+            v0 = 0.01f;
 
         }
 
         public Vector3 BallOnGround(float v0, Vector3 position, Vector3 dir, float[,] heights, string landtype)
         {
             Vector3 fPosition = position;
-            acc = new Vector3(0, 0, 0);
 
             if (this.hitGround(fPosition, heights))
             {
+                fPosition.Y = heights[(int)fPosition.X, (int)fPosition.Z] + r;
+
+                if (fPosition.X > 0 && fPosition.Z > 0)
+                {
+                    // heights difference between left and right
+                    float heightLRdiff = heights[(int)fPosition.X - 1, (int)fPosition.Z] - heights[(int)fPosition.X + 1, (int)fPosition.Z];
+                    // heights difference between front and back
+                    float heightFBdiff = heights[(int)fPosition.X, (int)fPosition.Z - 1] - heights[(int)fPosition.X, (int)fPosition.Z + 1];
+
+                    c = (float)Math.Sqrt(Math.Pow(heightLRdiff, 2) + Math.Pow(heightFBdiff, 2));
+                    cos = heightLRdiff / c;
+                    sin = heightFBdiff / c;
+
+                }
 
                 if (landtype.Equals("water"))
                 {
@@ -43,35 +65,29 @@ namespace Project2
                         //TODO: allow ball sink into water with static velocity
                         // unitil reaching river bed.
                 }
-                else if (landtype.Equals("moutain"))
-                {
-                    // heights difference between left and right
-                    float heightLRdiff = heights[(int)fPosition.X - 1, (int)fPosition.Z] - heights[(int)fPosition.X + 1, (int)fPosition.Z];
-                    // heights difference between front and back
-                    float heightFBdiff = heights[(int)fPosition.X, (int)fPosition.Z - 1] - heights[(int)fPosition.X, (int)fPosition.Z + 1];
-                }
                 else if (landtype.Equals("sand"))
                 {
-                    fPosition.Y = heights[(int)fPosition.X, (int)fPosition.Z];
-                    acc.Y = -0.01f;
+                    acc = -0.001f;
                 }
                 else
                 {
                     dir.Y = -1;
-                    acc.Y = -0.008f;
+                    acc = -0.008f;
                 }
 
                 // control the velocity of ball movement.
-                if (v0 > 0)
+                if (v0 > 0.0f)
                 {
-                    v0 += acc.Y;
+                    v0 += acc;
                 }
                 else
                 {
                     v0 = 0;
                 }
-                fPosition.X += dir.X * v0;
-                fPosition.Z += dir.Y * v0;
+
+                fPosition.X += dir.X * v0; 
+                fPosition.Z += dir.Z * v0;
+                //fPosition.Y += dir.Y * v0;
             }
 
             return fPosition;
@@ -86,13 +102,14 @@ namespace Project2
             // Comparing the height of current position of ball to 
             // the height of position in map. If height of ball is less or equal
             // to height of position in map, then set hitGround to true.
-            if((int) position.X >= 0 && (int) position.Z >= 0)
-            {
-                if (position.Y <= heights[(int)position.X, (int)position.Z])
+            //if((int) position.X >= 0 && (int) position.Z >= 0)
+            //{
+                if (position.Y <= heights[(int)position.X, (int)position.Z] + r)
                 {
+                    
                     hitGround = true;
                 }
-            }
+            //}
 
             return hitGround;
         }
@@ -113,16 +130,16 @@ namespace Project2
         {
             Vector3 position =  game.ball.position;
 
-
             if (hitGround(position, land.pHeights))
             {
-                game.ball.position = this.BallOnGround(v0, position, direction, land.pHeights, "sand");
+
+                position = this.BallOnGround(v0, position, direction, land.pHeights, "sand");
             }
             else
             {
                 position.Y -= v0;
-                //position.X -= v0;
-                //position.Z -= v0;
+                //position.X += v0;
+                //position.Z += v0;
             }
 
             game.ball.position = position;
